893d3001b19c32cad2dc499dd34e6a13d51b51d6
Raymond <gd.raymond@gmail.com>
2019-01-15 07:14:04 +1000

    add matplotlib

--- imdb-pre-embed-6.8.py
+++ imdb-pre-embed-6.8.py
@@ -90,4 +90,18 @@ model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['acc'])
 history=model.fit(x_train,y_train,epochs=10,batch_size=32,validation_data=(x_val,y_val))
 model.save_weights('pre_trained_glove_model.h5')
 
-import matplotlib.
+import matplotlib.pyplot as plt
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+epochs=range(1,len(acc)+1)
+plt.plot(epochs,acc,'bo',label='Training acc')
+plt.plot(epochs,val_acc,'b',label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.plot(epochs,loss,'bo',label='Training loss')
+plt.plot(epochs,val_loss,'b',label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
